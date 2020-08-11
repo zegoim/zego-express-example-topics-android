bbaa635abc59d3355d3257632880d5c511d17990
Patrick Fu <patrickfu@zego.im>
2020-08-11 15:05:15 +0800

    fix: Added appID config files
    Former-commit-id: 1635060248eb3473d0221539658599b9ac169660

---
+++ video/common/src/main/java/im/zego/common/GetAppIDConfig.java
@@ -0,0 +1,14 @@
+package im.zego.common;
+
+public class GetAppIDConfig {
+    // Apply AppID and AppSign from Zego
+    // https://console-express.zego.im/account/login
+
+    // for example:
+    //     public static final long appID = 123456789L;
+    public static final long appID = ;
+
+    // for example:
+    //    public static final String appSign = "0123456789012345678901234567890123456789012345678901234567890123";
+    public static final String appSign = ;
+}
